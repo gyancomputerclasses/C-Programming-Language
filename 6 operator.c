e31ba32c1f30612580c5c2fff0efd72e1cc8bf57
Gyan Computer Classes <90661582+gyancomputerclasses@users.noreply.github.com>
2026-09-18 08:33:46 +0530

    Create 6 operator.c

---
+++ 6 operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int num1 = 5, num2 = 8, sum, sub;
+    sum = num1 + num2;
+    printf("%d\n",sum);
+    printf("sum of two number - %d + %d = %d \n",num1, num2, sum);
+    sub = num2 - num1;
+    printf("sub of two number - %d - %d = %d",num2, num1, sub);
+}
