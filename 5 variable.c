26e0397ddc9589a1987fdc0a2927af12fb2dbba1
Gyan Computer Classes <90661582+gyancomputerclasses@users.noreply.github.com>
2026-09-18 08:07:37 +0530

    Create 5 variable.c

---
+++ 5 variable.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int num1 = 5;
+    printf("%d",num1);
+    
+    printf("\n");
+    float decimal = 5.4;
+    printf("%f",decimal);
+
+    printf("\n");
+    char str= 'C';
+    printf("%c",str);
+    return 0;
+}
