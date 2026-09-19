f84d03306c399c8272fff4b6e9429650062f61f3
Gyan Computer Classes <90661582+gyancomputerclasses@users.noreply.github.com>
2026-09-19 08:11:40 +0530

    Add grading system based on percentage

--- 8 elseif.c
+++ 8 elseif.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
 
 int main(){
-    int time = 16;
+    int per = 87;
 
-    if (time < 12) {
-      printf("Good morning.");
-    } else if (time < 18) {
-      printf("Good day.");
-    } else {
-      printf("Good evening.");
+    if(per>=90){
+        printf("Your per. %d and Grade A",per);
+    }else if(per >= 80){
+        printf("Your per. %d and Grade b",per);
+    }else if(per >= 70){
+        printf("Your per. %d and Grade c",per);
+    }else if(per >= 60){
+        printf("Your per. %d and Grade d",per);
+    }else if(per >= 50){
+        printf("Your per. %d and Grade e",per);
+    }else if(per >= 40){
+        printf("Your per. %d and Grade f",per);
+    }else if(per >= 30){
+        printf("Your per. %d and Grade g",per);
+    }else{
+        printf("Fail");
     }
 }
