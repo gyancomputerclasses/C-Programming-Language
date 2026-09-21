24d99a97dfc3605a8236a5147ec32abe5b3839b9
Gyan Computer Classes <90661582+gyancomputerclasses@users.noreply.github.com>
2026-09-21 08:00:34 +0530

    Create 9 nestedif.c

---
+++ 9 nestedif.c
@@ -0,0 +1,15 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int a = 4, b=3;
+    if(a>=2){
+        if(b>=5){
+            printf("Successfully Done");
+        }else{
+            printf("less then value 5");
+        }
+        }else{
+            printf("Less then value 2");
+        }
+}
