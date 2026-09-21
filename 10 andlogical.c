6bea887dbe566cc5619e5fa49a468f27f5a946c0
Gyan Computer Classes <90661582+gyancomputerclasses@users.noreply.github.com>
2026-09-21 08:09:25 +0530

    Create 10 andlogical.c

---
+++ 10 andlogical.c
@@ -0,0 +1,11 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int hindi=45,math=67,sci=89,sst=24,sanskrit=99;
+    if(hindi>=30 && math>=30 && sci >=30 && sst >= 30 && sanskrit >= 30){
+        printf("Pass");
+    }else{
+        printf("Fail");
+    }
+}
